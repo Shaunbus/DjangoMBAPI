c7c97d66e92033c6b36b62fbcde0a81a268dc574
Shaunbus <sjbucyk@live.ca>
2020-02-26 17:41:18 +0000

    basic sqlite db set up

--- msgs/models.py
+++ msgs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import RegexValidator, MinLengthValidator
 
 # Create your models here.
+
+class Msg(models.Model):
+    name = models.CharField(max_length=10, validators=[
+        RegexValidator('[A-Za-z]{2,}'),
+        MinLengthValidator(2)
+    ])
+    msg = models.CharField(max_length=20, validators=[
+        MinLengthValidator(3)
+    ])
+    class Meta:
+        ordering = ['-id']
